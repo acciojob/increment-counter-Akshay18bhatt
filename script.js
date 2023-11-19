e34cfcd773da16676641b6e4fcdd338df666fee3
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-19 12:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let p=document.getElementById("counter");
+let button= document.getElementById("incrementBtn");
+
+button.addEventListener("click", onclicking);
+
+function onclicking() {
+	alert(p.innerText);
+	
+	p.innerText++;
+	
+	
+}
